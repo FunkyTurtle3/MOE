cf22ad3647682d8b8b5bc90daed61087dc153228
FunkyTurtl3 <lasse2008s@icloud.com>
2025-04-06 18:04:04 +0200

    Initial Commit for this project

--- src/main/java/de/funkyturtle/moreofeverything/item/custom/MysteriousMaterial.java
+++ src/main/java/de/funkyturtle/moreofeverything/item/custom/MysteriousMaterial.java
@@ -1,9 +1,11 @@
 package de.funkyturtle.moreofeverything.item.custom;
 
+import de.funkyturtle.moreofeverything.component.MOEDataComponentTypes;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 
 import java.util.List;
@@ -14,7 +16,7 @@ public class MysteriousMaterial extends Item {
     private String component;
 
     public MysteriousMaterial(Properties pProperties, ItemStack insaneItem, String component) {
-        super(pProperties);
+        super(pProperties.rarity(Rarity.RARE).stacksTo(1).component(MOEDataComponentTypes.LOCKED.get(), Boolean.FALSE).durability(16));
         this.insaneItem = insaneItem;
         this.component = component;
     }
@@ -32,4 +34,5 @@ public class MysteriousMaterial extends Item {
         }
         super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     }
+
 }
